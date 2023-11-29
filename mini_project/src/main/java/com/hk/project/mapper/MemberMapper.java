28c2e7af17029ac93448882fbbb67095a3c210a7
insooj <insooj41@gmail.com>
2023-11-30 00:28:38 +0900

    Commit

---
+++ mini_project/src/main/java/com/hk/project/mapper/MemberMapper.java
@@ -0,0 +1,22 @@
+package com.hk.project.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.hk.project.dtos.MemberDto;
+
+@Mapper
+public interface MemberMapper {
+	
+	public boolean addUser(MemberDto dto);
+	
+	public String idChk(String id);
+	
+	public MemberDto loginUser(String id);
+	
+}
+
+
+
+
+
+
